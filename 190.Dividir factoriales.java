fcefb6b7d164d198c7f9df81918206a0fcaaaec1
nachomonllor <nachomonllorc@gmail.com>
2019-06-25 02:27:14 -0300

    agrego

---
+++ 190.Dividir factoriales.java
@@ -0,0 +1,43 @@
+﻿import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Stack;
+
+/**
+ *
+ * @author Usuario
+ */
+public class JavaApplication19 {
+
+     
+    static long factorial(long num, long den) {
+        long prod = 1;
+        for(long i =den+1; i <= num; i++) {
+            prod *= i;
+        }
+        return prod;
+    }
+   
+    
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        
+       // System.out.println(Long.MAX_VALUE);
+        
+        String s;
+        while( (s = br.readLine()) != null) {
+            String[] split = s.split(" ");
+            long num = Integer.parseInt(split[0]);
+            long den = Integer.parseInt(split[1]);
+            if(num < den) {
+                break;
+            }
+            System.out.println( factorial(num, den));
+        }
+    }
+
+}
